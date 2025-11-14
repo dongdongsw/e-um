98a72637633a6863af2ef17538276779bddfd510
jieunpyo <jieunhye1121@naver.com>
2025-11-15 03:07:35 +0900

    PayModel 생성

---
+++ src/main/java/com/eum/pay/model/PayModel.java
@@ -0,0 +1,26 @@
+package com.eum.pay.model;
+
+import com.eum.main.vo.BoardVO;
+import com.eum.pay.dao.PayDAO;
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class PayModel {
+	@RequestMapping("main/orders.eum")
+   public String ordersData(HttpServletRequest request,
+		   HttpServletResponse response)
+   {
+		String b_op_id=request.getParameter("b_op_id");
+		
+		BoardVO orders_vo=PayDAO.ordersData(Integer.parseInt(b_op_id));
+		
+		request.setAttribute("orders_vo", orders_vo);
+		
+		request.setAttribute("main_jsp", "../pay/order_payment.jsp");
+		return "../main/main.jsp";
+   }
+}
