aec896d8db00696283c512d4762555a77799c55b
jieunpyo <jieunhye1121@naver.com>
2025-11-15 02:54:59 +0900

    Update ReviewVO.java

--- src/main/java/com/eum/main/vo/ReviewVO.java
+++ src/main/java/com/eum/main/vo/ReviewVO.java
@@ -14,7 +14,7 @@ import lombok.Data;
  */
 @Data
 public class ReviewVO {
-	private int u_s_id,review_count;
+	private int u_s_id,r_count;
 	private String b_review_id,b_id,u_id,b_review_createdat,b_review_content,b_review_score;
 
 
