80f06f50ee6199a8e1982455a6862d62883297e6
jieunpyo <jieunhye1121@naver.com>
2025-11-16 11:39:09 +0900

    o_id 수정

--- src/main/java/com/eum/main/vo/OrdersVO.java
+++ src/main/java/com/eum/main/vo/OrdersVO.java
@@ -12,7 +12,7 @@ import lombok.Data;
  */
 @Data
 public class OrdersVO {
-	private int b_op_id,o_total_price;
-	private String o_id,o_status,o_u_id;
+	private int b_op_id,o_total_price,o_u_id;
+	private String o_id,o_status;
 	private Date o_createdat,o_updatedat;
 }
